f09e8b569805f1d34260b7f3e23409aa63096b49
Sankeerthana-S <sankeerthana172000@gmail.com>
2025-05-03 17:14:10 +0530

    Basic Setup

---
+++ src/basic-syntax.py
@@ -0,0 +1,18 @@
+# A comment line is a non-executable line in a code file
+# A single command line begins with "#"- hashtag
+"""
+This is a multi comment line
+Useful in comment specific parts of a code
+or write some description
+"""
+
+# Printing in Python
+# simple with predefined / built-in function in Python "Print"
+# Example
+print("Hi Python Learner!")
+
+# Importance of Indentation
+# Python doesn't have curly braces {} to segregate code block
+# It identifies code blocks through proper indentation
+# if True:
+#     print("Another Hi for Python Learner!")
